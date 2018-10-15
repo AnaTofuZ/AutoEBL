6430b14067051d4afbe1d710ffeac714d33a6bef
Takahiro SHIMIZU <anatofuz@gmail.com>
2018-10-15 22:34:34 +0900

    define chromedp tasks and Options
    琉大図書館のCSSセレクタなどをclickするように実装した

--- AutoEBL.go
+++ AutoEBL.go
@@ -1,27 +1,101 @@
 package AutoEBL
 
 import (
-	"fmt"
+	"context"
+	"github.com/chromedp/chromedp"
+	"github.com/pkg/errors"
+	"log"
 	"os"
+	"time"
 )
 
 const (
 	version = "0.01"
+	url     = "https://opac.lib.u-ryukyu.ac.jp/portal/portal/selectLogin"
 )
 
 type AutoEBL struct {
-	URL  string
-	args []string
+	username string
+	password string
 }
 
 func New() *AutoEBL {
-	return &AutoEBL{
-		URL:  "http://www.lib.u-ryukyu.ac.jp",
-		args: os.Args[1:],
-	}
+	return &AutoEBL{}
 }
 
 func (auebl *AutoEBL) Run() error {
-	_, err := fmt.Println("Hello")
+	auebl.setEnvParams()
+	err := auebl.parseOptions(os.Args[1:])
+
+	//if err != nil { return err }
+
+	if !auebl.Define() {
+		return errors.New("failed to not input username and password")
+	}
+
+	auebl.doEBL()
+
 	return err
+}
+
+// setEnvParams is set username and password for  enviroment values
+// this function didn't happen exception by that os.Getenv return  not return erorr
+func (auebl *AutoEBL) setEnvParams() {
+	auebl.check_set(&auebl.username, os.Getenv("AUEBL_USERNAME"))
+	auebl.check_set(&auebl.password, os.Getenv("AUEBL_PASSWORD"))
+}
+
+func (auebl *AutoEBL) check_set(auebl_conf *string, set_val string) {
+	if set_val == "" {
+		return
+	}
+	*auebl_conf = set_val
+}
+
+func (auebl *AutoEBL) Define() bool {
+	if auebl.username == "" || auebl.password == "" {
+		return false
+	}
+	return true
+}
+
+func (auebl *AutoEBL) doEBL() error {
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	chrome, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+
+	if err != nil {
+		return err
+	}
+
+	err = chrome.Run(ctxt, auebl.sendkeys())
+
+	if err != nil {
+		return err
+	}
+
+	chrome.Shutdown(ctxt)
+
+	return err
+}
+
+func (auebl *AutoEBL) sendkeys() chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		// LDAP login
+		chromedp.Click("javascript:$('input[name=lang]').val('ja');$('#frmSso').submit();"),
+		chromedp.SetValue(`#username`, auebl.username),
+		chromedp.SetValue(`#password`, auebl.password),
+		chromedp.Click("_eventId_proceed"),
+		// library menue
+		chromedp.WaitVisible("#menu-inner"),
+		chromedp.Click(`//*[@id="menu-inner"]/ul/li[2]/a`),
+		chromedp.Click(`//*[@id="menu-inner"]/ul/li[2]/ul/li[1]/a`),
+		// extention
+		chromedp.WaitVisible(`//*[@id="lblTh1"]`),
+		chromedp.Click(`#itemCheckA000`),
+		chromedp.Click(`//*[@id="lblExtend"]`),
+		chromedp.Sleep(30 * time.Second),
+	}
 }
--- Options.go
+++ Options.go
@@ -1,6 +1,7 @@
 package AutoEBL
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jessevdk/go-flags"
@@ -8,12 +9,13 @@ import (
 )
 
 type Options struct {
-	Help    bool `short:"h" long:"helo" description:"show this message"`
-	Version bool `short:"vj" long:"version" description:"show the version"`
+	Help     bool   `short:"h" long:"help" description:"show this message"`
+	Version  bool   `short:"v" long:"version" description:"show the version"`
+	Username string `short:"u" long:"username" description:"set your user name ex.e1557xx"`
+	Password string `short:"p" long:"password" description:"set your password for ie"`
 }
 
-func (opts *Options) parse(argv []string) ([]string, error) {
-	parser := flags.NewParser(opts, flags.PrintErrors)
+func (opts *Options) parse(parser *flags.Parser, argv []string) ([]string, error) {
 	args, err := parser.ParseArgs(argv)
 
 	if err != nil {
@@ -23,6 +25,27 @@ func (opts *Options) parse(argv []string) ([]string, error) {
 	return args, nil
 }
 
-func (auebl *AutoEBL) parseOptions() {
-	return
+func (autoebl *AutoEBL) parseOptions(osargs []string) error {
+	var opts Options
+	parser := flags.NewParser(&opts, flags.PrintErrors)
+	args, err := opts.parse(parser, osargs)
+
+	if err != nil {
+		return err
+	}
+
+	if opts.Version {
+		fmt.Println("Auto EBL", version)
+		os.Exit(0)
+	}
+
+	if opts.Help || (!(autoebl.Define()) && len(args) == 0) {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(0)
+	}
+
+	autoebl.check_set(&autoebl.username, opts.Username)
+	autoebl.check_set(&autoebl.password, opts.Password)
+
+	return nil
 }
